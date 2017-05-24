9ba30c50e09363324fd6c23015fad12d96c5d279
peter.gerken <peter.gerken@erdfisch.de>
2017-05-24 17:00:36 +0200

    Add wrapper and change some css so that the sidebar will stay sticky on the right position

--- l10n.js
+++ l10n.js
@@ -6,7 +6,7 @@
         this
             .wrap('<div id="l10n-sidebar"><div id="l10n-content"></div></div>');
         $('#l10n-sidebar')
-            .prepend('<div id="slide" class="sticky"><h2>Translation Tools</h2></div>');
+            .prepend('<div id="l10n-sidebar-container"><div id="slide" class="sticky"><h2>Translation Tools</h2></div></div>');
         $('#slide')
             .append('<div id="block"><h3>Suggestion</h3>block block block block block block block block block block block block block block </div>');
         $('#slide')
